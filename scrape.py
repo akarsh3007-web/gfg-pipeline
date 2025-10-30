8d21f69c4a0472aacb5326d910a4e2e2b8f8d740
Ak8933 <akarshkatiyarak123@gmail.com>
2025-10-30 21:53:09 +0530

    fix

--- scrape.py
+++ scrape.py
@@ -151,7 +151,7 @@ def run_pipeline():
     save_cpp_to_file(cpp_code, problem_name)
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
 
